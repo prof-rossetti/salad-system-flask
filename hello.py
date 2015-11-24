105c15f331e27f5cea8e727ab811db9ffc482caa
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-11-23 20:36:48 -0500

    routing form submission post requests; extending template usage

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 
 import code # to debug: `code.interact(local=locals())`
 import os
-from flask import Flask, render_template #request, session, g, redirect, url_for, abort, flash
+from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash
 from flaskext.mysql import MySQL # https://github.com/cyberdelia/flask-mysql
 
 #
@@ -14,6 +14,7 @@ except KeyError as e:
     DB_ROOT_PASSWORD = "" # most students' root user doesn't have a password
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24) # to facilitate sessions and flash
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = DB_ROOT_PASSWORD
 app.config['MYSQL_DATABASE_DB'] = 'salad_db'
@@ -28,16 +29,43 @@ mysql.init_app(app)
 
 @app.route("/")
 def hello():
-    return "<h1>Welcome, Regional Manager.</h1><a href='/menu'>View Menu</a>"
+    return render_template('index.html')
 
 @app.route("/menu")
-def menu():
+def menu_items():
     cursor = mysql.connect().cursor()
     cursor.execute("SELECT * from menu_items ORDER BY id LIMIT 10;")
     menu_items = [dict(title=row[2], description=row[6]) for row in cursor.fetchall()]
-    return render_template('menu.html', menu_items=menu_items)
+    return render_template('menu-items/index.html', menu_items=menu_items)
 
+@app.route("/form")
+def edit_menu_item():
+    return render_template('menu-items/form.html')
 
+@app.route("/new", methods=['POST'])
+def new_menu_item():
+
+    #category = request.form['category']
+    #title = request.form['title']
+    #calories = request.form['calories']
+    #description = request.form['description']
+    #try:
+    #    gluten_free = True if form['gluten_free'] else False
+    #except KeyError as e:
+    #    gluten_free = False
+    #try:
+    #    vegan_safe = True if form['vegan_safe'] else False
+    #except KeyError as e:
+    #    vegan_safe = False
+    #calories = int(calories)
+    #gluten_free = int(gluten_free)
+    #vegan_safe = int(vegan_safe)
+
+    #cursor = mysql.connect().cursor()
+    #cursor.execute("SELECT * from menu_items ORDER BY id LIMIT 10;")
+
+    flash('Thanks for adding a menu item.')
+    return redirect(url_for('menu_items'))
 
 
 
